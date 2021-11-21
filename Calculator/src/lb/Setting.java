0ad7128dc2075407631534210fffbf0572175fbe
igor <igor@igor-VirtualBox>
2021-11-21 16:44:27 +0400

    class s nastroykami

--- Calculator/src/lb/Setting.java
+++ Calculator/src/lb/Setting.java
@@ -1,5 +1,36 @@
 package lb;
 
-public class Setting {
+import javax.swing.*;
 
+public class Setting extends Asetting{
+    public int TypeOfProperty;//тип имущества
+    public float CostOfProperty;//стоимость имущества
+    public int[] p=new int [7];//массив сохраняющий параметры пользователя из кнопок (от чего страховать)
+    @Override
+    public void setCost(JTextField field) throws NumberFormatException {
+        try {
+            CostOfProperty = Float.parseFloat(field.getText());
+        } catch (NumberFormatException nfe) {
+            throw new NumberFormatException();
+        }
+    }
+    @Override
+    public void set(int x) {
+        p[x]=1;
+    }
+
+    @Override
+    public void setType(int x) {
+        TypeOfProperty=x;
+    }
+
+    @Override
+    public void reset(int x) {
+        p[x]=0;
+    }
+
+    @Override
+    public int get(int x) {
+        return p[x];
+    }
 }
