9aa4b36bce20cf918a7337a870ecd60f55557a14
tim <tim@tim-VirtualBox>
2021-12-01 17:10:09 +0400

    Ura travis zarabotal (test)

--- Calculator/src/lb/ACalculate.java
+++
@@ -1,7 +0,0 @@
-package lb;
-
-public abstract class ACalculate {
-	public static float polis=0;//итоговая стоимость
-    protected  float[][] koefficiets;  //коэффициенты для расчета
-    public abstract  float Cal(Setting s);
-}
