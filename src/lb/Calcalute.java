667586fe0358214455290475b0caad364dd5e179
igor <igor@igor-VirtualBox>
2021-12-05 14:35:14 +0400

    dobavil konstruktor

--- src/lb/Calcalute.java
+++ src/lb/Calcalute.java
@@ -6,7 +6,13 @@ public class Calcalute extends ACalculate{
                 {0.0029f, 0.0005f, 0.00004f, 0.00006f, 0.0004f, 0.0001f, 0.000015f},
                 {0.002f, 0.0006f, 0.0001f, 0.000017f, 0.0007f, 0.0001f, 0.00005f}}; //коэффициенты для расчета
     };
-
+    public Calcalute(float x[][]) {
+    	for (int i=0;i<2;i++)
+    	{for(int j=0;j<7;j++)
+    	{koefficiets[i][j]=x[i][j];
+    	};
+    	};
+    }
     @Override
     public  float Cal(Setting s) {
         float sum = 0;
